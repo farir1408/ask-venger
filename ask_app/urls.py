a201a06fe4ab380d6058ae08d4e785c567adac2b
farir1408 <farir1881@gmail.com>
2017-12-15 13:52:28 -0800

    add dz3

--- ask_app/urls.py
+++ ask_app/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^question/', views.question),
-    url(r'^ask/', views.ask),
-    url(r'^login/', views.login),
-    url(r'^register/', views.register),
-    url(r'^params/', views.params)
+    url(r'^$', views.index, name='index'),
+    url(r'^question/(?P<question_id>[0-9]+)?/?$', views.question, name='question'),
+    url(r'^ask/', views.ask, name='ask'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^register/', views.register, name='signup'),
+    url(r'^params/', views.params, name='params')
 ]
